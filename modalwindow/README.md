b7feab3305e405bebc144cb94b0c5c0862c3836b
operupolnomochennaya <migal.polina@gmail.com>
2025-07-23 21:12:41 +0300

    modal window

---
+++ modalwindow/README.md
@@ -0,0 +1,6 @@
+# ModalWindow
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/operupolnomochennaya/pen/ZYbObqq](https://codepen.io/operupolnomochennaya/pen/ZYbObqq).
+
---
+++ modalwindow/src/script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('modal');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
+
+openModalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
